408aac716ae6225a249ed948c71e6761049e4cd5
yonatankumsa <guitaristyona@gmail.com>
2022-08-24 19:04:54 -0400

    created a lot of styling with chief cards

---
+++ recipe-app/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+export default function Footer(){
+    return (
+        <div className="footer container">
+            <div className="footer-section">
+                
+                <p>FoodiesHub is a place where you can please your soul and tummy with delicious food recepies of all cuisine. And our service is absolutely free.</p>
+            
+            </div>
+            <div className="footer-section">
+                <p className="title">Contact Me</p>
+                <p>yonatankumsa99@gmail.com</p>
+  
+              
+            </div>
+            <div className="footer-section">
+                <p className="title">Socials</p>
+                <p>Facebook</p>
+                <p>Twitter</p>
+                <p>Instagram</p>
+            </div>
+        </div>
+    )
+}
